d591415d5655595bba36c33073f0d5471854769f
vodobryshkin <dobryshkinvladimir@yandex.ru>
2026-05-06 19:24:38 +0300

    добавил jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("jacoco")
 }
 
 group = "com.github.vodobryshkin"
@@ -18,10 +19,24 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 subprojects {
     plugins.apply("java")
+    plugins.apply("jacoco")
+
+    jacoco {
+        toolVersion = "0.8.11"
+    }
 
     repositories {
         mavenCentral()
@@ -30,5 +45,18 @@ subprojects {
     dependencies {
         testImplementation("org.assertj:assertj-core:3.27.7")
         testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        finalizedBy(tasks.withType<JacocoReport>())
+    }
+
+    tasks.withType<JacocoReport> {
+        dependsOn(tasks.withType<Test>())
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
     }
 }
